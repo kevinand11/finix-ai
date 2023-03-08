9a5f73d650266620fe208075e15489d5f1b06e14
kevinand11 <kevinfizu@gmail.com>
2023-03-08 20:32:17 +0100

    gh-pages

---
+++ _nuxt/composables.39c51967.js
@@ -0,0 +1,1 @@
+import{s}from"./entry.58d054f6.js";function t(e,u){return s()._useHead(e,u)}export{t as u};
--- src/application/components/Navbar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div class="flex items-center justify-between gap-6 py-4 px-6 md:py-6 md:px-16">
-		<img src="@/assets/images/logo.svg" class="h-[35px] md:h-[50px]" alt="FINIX AI">
-		<a href="/FINIX_LITE_PAPER.pdf">
-			<button>Whitepaper</button>
-		</a>
-	</div>
-</template>
